90556c5d703a389696798efce45fa2c515db8400
George <loaizageorge@gmail.com>
2016-08-04 17:49:31 -0400

    Able to perform calculations. Working if used correctly, no cases included (eg. first button pressed is add, consecutive operators and decimals). Working on getting calc to restart if a number is pressed after a calculation is done rather than keep calculating with the same number(if a operator is pressed).

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
+    var number = ["0"];
+    start(number);
+   
+});
+
+function evaluate(number){
+    var temp = number.join('');
+    var temp1 = eval(temp);
+    $("#screen").html(temp1);
+    number = [""];
+    number.push(temp1);
+    start(number);
+}
+
+function start(number){
+    $("#screen").html(number);
+    
     $(".regular-btn").click(function(){
-   alert(this.innerHTML);
+       
+        if($(this).html()=="="){
+           evaluate(number);
+        } else{
+        $("#screen").html(this.innerHTML);
+        number.push($(this).html());
+        }
+        console.log(number);
+    
+        
 });
-});
+}
+
