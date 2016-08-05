e6ba61c85ebb9ccd082b98bfe9d5e6eb5e609cac
George <loaizageorge@gmail.com>
2016-08-04 22:02:56 -0400

    Cases included. Can't spam an operator or a decimal, have an operator after a decimal(ie .+), have more than one decimal per number (eg. 3.3.2). Last thing to do functionality wise is to have CE clear the entry(eg. 3+9 CE --> 3+ )

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,90 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
     var stringArr = [];
-    var isResult = false;
-    $("#screen").html("0");
+    //FLAGS
+    var isResult = false; //Used to decide whether to chain or restart depending on whether op or number is pressed(after computation).
+    var op_recent = false; //Used to disable op spamming(eg. 3+++)
+    var decimal_recent = false; // Used to disable decimal spam(eg. 3...3);
+    var isFloat = false;
     
-    $(".num-btn").click(function(){
-        if(isResult){
+    $("#screen").html("0");
+    $(".num-btn").click(function () {
+        if (isResult) {
             stringArr = [];
             stringArr.push($(this).html());
             isResult = false;
             //alert("New Calculation");
-             $("#screen").html(stringArr);
+            $("#screen").html(stringArr);
+            
+            op_recent = false;
+            decimal_recent = false;
+            
         }
-        else{
-           // alert("chain");
+        else {
+            // alert("chain");
             stringArr.push($(this).html());
-             $("#screen").html(stringArr);
+            $("#screen").html(stringArr);
+            
+            op_recent = false;
+            decimal_recent = false;
+        }
+    });
+    
+    $("#decimal-btn").click(function(){
+       if(decimal_recent||isFloat){
+           return false;
+       }
+        
+        else{
+            stringArr.push($(this).html());
+            $("#screen").html(stringArr);
+            
+            op_recent = false;
+            decimal_recent = true;
+            isFloat = true;
         }
         
     });
     
-    $(".op-btn").click(function(){
-       //alert("operation");
-        stringArr.push($(this).html());
-       $("#screen").html(stringArr);
-        isResult = false;
-       
+    $(".op-btn").click(function () {
+        //alert("operation");
+        if (op_recent ||decimal_recent|| stringArr.length == 0) {
+            return false;
+        }
+        else {
+            stringArr.push($(this).html());
+            $("#screen").html(stringArr);
+            
+            isResult = false;
+            op_recent = true;
+            decimal_recent = false;
+            isFloat = false;
+        }
     });
-    
-    $(".equal-btn").click(function(){
+    $(".equal-btn").click(function () {
         var temp = stringArr.join('');
         var result = eval(temp);
         stringArr = temp.split(" ");
         $("#screen").html(result);
-        isResult = true;
         stringArr = [];
         stringArr.push(result);
+        op_recent = false;
+        isResult = true;
+        decimal_recent = false;
     });
-    
-    $("#ac-btn").click(function(){
+    $("#ac-btn").click(function () {
         stringArr = [];
         $("#screen").html("0");
         
+        op_recent = false;
+        isResult = true;
+        decimal_recent = false;
     });
-    
-    $("#ce-btn").click(function(){
+    $("#ce-btn").click(function () {
         stringArr = [];
         $("#screen").html("0");
         
+        op_recent = false;
+        isResult = false;
+        decimal_recent = false;
     });
-   
 });
-   
-    
-  
-   
-
-
-
-
