95e3c41b5b74090cb2ff9e2ae89fba3c8ebec4c0
George <loaizageorge@gmail.com>
2016-08-06 21:50:38 -0400

    Stop numbers from going past the screen if they are more than 12 digits with overflow function.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,7 @@ $(document).ready(function () {
     var decimal_recent = false; // Used to disable decimal spam(eg. 3...3);
     var isFloat = false;
     var buildingNumber = false;
-    
     $("#screen").html("0");
-    
     $(".num-btn").click(function () {
         if (isResult) {
             expression = [];
@@ -18,33 +16,29 @@ $(document).ready(function () {
             countDigits = 0;
             expression.push($(this).html());
             $("#screen").html(expression);
-            
             countDigits++;
             isResult = false;
             op_recent = false;
             decimal_recent = false;
             buildingNumber = true;
-            
         }
         else {
             // alert("chain");
-            expression.push($(this).html());
-            $("#screen").html(expression);
-            
-            countDigits++;
-            op_recent = false;
-            decimal_recent = false;
-            buildingNumber = true;
-            
+            if (!overflowCheck(num_of_digits, countDigits)) {
+                expression.push($(this).html());
+                $("#screen").html(expression);
+                countDigits++;
+                op_recent = false;
+                decimal_recent = false;
+                buildingNumber = true;
+            }
         }
     });
-    
-    $("#decimal-btn").click(function(){
-       if(decimal_recent||isFloat){
-           return false;
-       }
-        
-        else{
+    $("#decimal-btn").click(function () {
+        if (decimal_recent || isFloat) {
+            return false;
+        }
+        else {
             expression.push($(this).html());
             $("#screen").html(expression);
             countDigits++;
@@ -52,91 +46,98 @@ $(document).ready(function () {
             decimal_recent = true;
             isFloat = true;
             buildingNumber = true;
-            
         }
-        
     });
-    
     $(".op-btn").click(function () {
         //alert("operation");
-        if (op_recent ||decimal_recent|| expression.length == 0) {
+        if (op_recent || decimal_recent || expression.length == 0) {
             return false;
         }
         else {
             expression.push($(this).html());
             $("#screen").html(expression);
-            
             isResult = false;
             op_recent = true;
             decimal_recent = false;
             isFloat = false;
-            
             //Store amount of digits in prev number and also a 1 to delete operators in CE click.
             num_of_digits.push(countDigits);
             countDigits = 0;
             num_of_digits.push(1);
             buildingNumber = false;
-            
         }
     });
     $(".equal-btn").click(function () {
         var temp = expression.join('');
         var result = eval(temp);
-        var temp1 = result;
-        $("#screen").html(result);
+        countDigits = result.toString().length;
         expression = [];
         expression.push(result);
-        
         num_of_digits = [];
-        
-        
-        countDigits = result.toString().length;
-        op_recent = false;
-        isResult = true;
-        decimal_recent = false;
-        buildingNumber = true;
+        if (!overflowCheck(num_of_digits, countDigits)) {
+            $("#screen").html(result);
+            op_recent = false;
+            isResult = true;
+            decimal_recent = false;
+            buildingNumber = true;
+        }
     });
     $("#ac-btn").click(function () {
         expression = [];
-        num_of_digits=[];
-        countDigits =0;
+        num_of_digits = [];
+        countDigits = 0;
         $("#screen").html("0");
         op_recent = false;
         isResult = true;
         decimal_recent = false;
     });
-    
     $("#ce-btn").click(function () {
-        if(num_of_digits.length==1||num_of_digits.length==0){
+        if (num_of_digits.length == 1 || num_of_digits.length == 0) {
             expression = [];
             $("#screen").html("0");
             op_recent = false;
             isResult = false;
             decimal_recent = false;
-            num_of_digits=[];
+            num_of_digits = [];
             isFloat = false;
-            countDigits =0;
-            
-        } else{
-            if(buildingNumber){
+            countDigits = 0;
+        }
+        else {
+            if (buildingNumber) {
                 num_of_digits.push(countDigits);
                 countDigits = 0;
-                buildingNumber=false;
+                buildingNumber = false;
                 decimal_recent = false;
-                
-                
             }
-                alert(num_of_digits);
-                var amount_to_delete = num_of_digits.pop();
-                var position = expression.length - amount_to_delete;
-                expression.splice(position,amount_to_delete);
-                $("#screen").html(expression);
-                buildingNumber=false;
-                op_recent = false;
-                decimal_recent = false;
-            
-            
+            var amount_to_delete = num_of_digits.pop();
+            var position = expression.length - amount_to_delete;
+            expression.splice(position, amount_to_delete);
+            $("#screen").html(expression);
+            buildingNumber = false;
+            op_recent = false;
+            decimal_recent = false;
         }
     });
 });
 
+function overflowCheck(num_of_digits, count) {
+    if (num_of_digits.length == 0) {
+        var length = count;
+        if (length >= 12) {
+            $('#ac-btn').trigger('click');
+            $("#screen").html("OVERFLOW");
+            return true;
+        }
+    }
+    else {
+        var temp = num_of_digits.reduce(function (a, b) {
+            return a + b;
+        });
+        var length = temp;
+        if (length >= 12) {
+            $('#ac-btn').trigger('click');
+            $("#screen").html("OVERFLOW");
+            return true;
+        }
+    }
+}
